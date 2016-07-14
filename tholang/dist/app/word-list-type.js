bdb3f9373d4a508aaf833674ea35df1258fe4c39
Sathyanarayanan Saravanamuthu <ssaravanamuthu@vmware.com>
2016-07-14 17:48:29 +0530

    prod build

--- tholang/dist/app/word-list-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var WordListType = (function () {
-    function WordListType() {
-    }
-    return WordListType;
-}());
-exports.WordListType = WordListType;
-//# sourceMappingURL=word-list-type.js.map
