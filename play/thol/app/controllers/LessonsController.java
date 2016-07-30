956c4a6501fc99a6418fa293616a1258cb1f305d
Adhithya Rajagopalan <adhithya.rajagopalan@sap.com>
2016-07-30 12:31:29 +0530

    Corrected LessonsController

--- play/thol/app/controllers/LessonsController.java
+++ play/thol/app/controllers/LessonsController.java
@@ -1,16 +1,23 @@
 package controllers;
 
 import akka.actor.ActorSystem;
-import javax.inject.*;
+import jdk.nashorn.internal.runtime.JSONFunctions;
+import org.omg.CORBA.OMGVMCID;
 import play.*;
 import play.mvc.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.TimeUnit;
-import scala.concurrent.duration.Duration;
+import play.libs.Json;
 import scala.concurrent.ExecutionContextExecutor;
 import services.FileService;
+
+import javax.inject.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+import java.util.List;
+
+
 
 /**
  * This controller contains an action that demonstrates how to write
@@ -18,14 +25,14 @@ import services.FileService;
  * asynchronously delay sending a response for 1 second.
  *
  * @param actorSystem We need the {@link ActorSystem}'s
- * {@link Scheduler} to run code after a delay.
- * @param exec We need a Java {@link Executor} to apply the result
- * of the {@link CompletableFuture} and a Scala
- * {@link ExecutionContext} so we can use the Akka {@link Scheduler}.
- * An {@link ExecutionContextExecutor} implements both interfaces.
+ *                    {@link Scheduler} to run code after a delay.
+ * @param exec        We need a Java {@link Executor} to apply the result
+ *                    of the {@link CompletableFuture} and a Scala
+ *                    {@link ExecutionContext} so we can use the Akka {@link Scheduler}.
+ *                    An {@link ExecutionContextExecutor} implements both interfaces.
  */
 @Singleton
-public class WordsController extends Controller {
+public class LessonsController extends Controller {
 
     private final ActorSystem actorSystem;
     private final ExecutionContextExecutor exec;
@@ -33,31 +40,58 @@ public class WordsController extends Controller {
     private final String baseLocation;
 
     @Inject
-    public WordsController(ActorSystem actorSystem, ExecutionContextExecutor exec, FileService fileService) {
-      this.actorSystem = actorSystem;
-      this.exec = exec;
-      this.fileService = fileService;
-      this.baseLocation = "/var/thol/data/";
+    public LessonsController(ActorSystem actorSystem, ExecutionContextExecutor exec, FileService fileService) {
+        this.actorSystem = actorSystem;
+        this.exec = exec;
+        this.fileService = fileService;
+        this.baseLocation = "/var/thol/data/";
     }
 
-    public CompletionStage<Result> words() {
+    public CompletionStage<Result> lessons() {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                return Results.ok(fileService.getFileContent(baseLocation+"old_words/words.json"));
-            } catch(Exception e) {
-                return Results.internalServerError();
+                String lessonsDirectory = baseLocation + "lessons";
+                System.out.println(new File(lessonsDirectory).getAbsolutePath());
+
+                List<String> lessonNames = fileService.getDirectoryContent(lessonsDirectory);
+
+                HashMap<String, List<String>> lessonsMap = new HashMap<String, List<String>>();
+                lessonsMap.put("lessons", lessonNames);
+
+                return Results.ok(Json.toJson(lessonsMap));
+            } catch (Exception e) {
+                return Results.internalServerError("Internal Server Error");
             }
-        });        
+        });
     }
 
-    public CompletionStage<Result> word(String selWord){
+    public CompletionStage<Result> lessonContent(String lesson) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                return Results.ok(fileService.getFileContent(baseLocation+"old_words/words/"+java.net.URLDecoder.decode(selWord, "UTF-8")+".json"));
-            } catch(Exception e) {
-                return Results.internalServerError();
+                String lessonDirectory = baseLocation + "lessons" + "/" + lesson;
+                System.out.println(new File(lessonDirectory).getAbsolutePath());
+
+                return Results.ok(fileService.getFileContent(lessonDirectory + "/" + "content.json"));
+
+            } catch (Exception e) {
+                return Results.internalServerError("Internal Server Error");
             }
-        });   
+        });
     }
+
+    public CompletionStage<Result> lessonEvaluation(String lesson) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                String lessonDirectory = baseLocation + "lessons" + "/" + lesson;
+                System.out.println(new File(lessonDirectory).getAbsolutePath());
+
+                return Results.ok(fileService.getFileContent(lessonDirectory + "/" + "evaluation.json"));
+
+            } catch (Exception e) {
+                return Results.internalServerError("Internal Server Error");
+            }
+        });
+    }
+
 
 }
