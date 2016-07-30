a35f7933ba8cf27be7d9a022e89b9f7616c38517
Adhithya Rajagopalan <adhithya.rajagopalan@sap.com>
2016-07-30 11:58:26 +0530

    Add lessons API
    /lessons
    /lessons/:lesson/content
    /lessons/:lesson/evaluate

--- play/thol/app/services/FileService.java
+++ play/thol/app/services/FileService.java
@@ -3,12 +3,25 @@ package services;
 import javax.inject.*;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Singleton
 public class FileService {
-	public String getFileContent(String absolutePath) throws Exception{
-		System.out.println(absolutePath);
-		return new Scanner(new File(absolutePath)).useDelimiter("\\Z").next();
-	}
+    public String getFileContent(String absolutePath) throws Exception {
+        System.out.println(absolutePath);
+        return new Scanner(new File(absolutePath)).useDelimiter("\\Z").next();
+    }
+
+    public List<String> getDirectoryContent(String absolutePath) throws Exception {
+        System.out.println(absolutePath);
+        File directory = new File(absolutePath);
+        List<File> subdirectories = Arrays.asList(directory.listFiles());
+
+        Stream<String> subdirectoryNamesStream = subdirectories.stream().map((subdirectory) -> subdirectory.getName());
+
+        List<String> subdirectoryNames = subdirectoryNamesStream.collect(Collectors.toList());
+        return subdirectoryNames;
+    }
 }
