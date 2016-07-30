641c9a94829eda3bec4863944407d5d71c86fd33
Sathyanarayanan Saravanamuthu <ssaravanamuthu@vmware.com>
2016-07-30 15:55:48 +0530

    Adding two more components

---
+++ tholang/src/app/components/verifytext/verifytext.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit, OnChanges, Output, EventEmitter, SimpleChange} from '@angular/core';
+
+@Component({
+	selector: 'verify-text',
+	templateUrl: './app/verifytext/verifytext.component.html'
+})
+
+export class VerifyTextComponent {
+	@Input()
+	question:String;
+
+	@Input()
+	answer:String;
+
+	currentAnswer:String;
+
+	status:Boolean = false;
+
+	@Output()
+	oncomplete = new EventEmitter();
+
+	answerChange(ans:String){
+		if(ans===this.answer){
+			this.status = true;
+			this.oncomplete.emit(true);
+		}else{
+			this.status = false;
+		}
+	}
+
+	showAnswer(){
+		this.currentAnswer = this.answer;
+		this.status = true;
+		this.oncomplete.emit(false);
+	}
+
+}
