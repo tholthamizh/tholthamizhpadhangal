77a78935a0a31219eab3fcd4440679206f1b571a
Adhithya Rajagopalan <adhithya.rajagopalan@sap.com>
2016-07-31 15:27:07 +0530

    css for Lessons

--- tholang/src/app/components/lessons/lessons.component.ts
+++ tholang/src/app/components/lessons/lessons.component.ts
@@ -14,6 +14,7 @@ import {EvaluationComponent} from '../evaluation/evaluation.component';
 @Component({
 	selector : 'lessons',
 	templateUrl : './app/components/lessons/lessons.template.html',
+	styleUrls: ['./app/components/lessons/lessons.css'],
 	directives: [GridList, EvaluationComponent]
 })
 
