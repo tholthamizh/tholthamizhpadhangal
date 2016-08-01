c6d33a74847e430b72a85d6c30e225d17a8a9acd
Sathyanarayanan Saravanamuthu <ssaravanamuthu@vmware.com>
2016-08-01 10:52:40 +0530

    Fixing merge conflicts

--- tholang/src/app/random-word.component.ts
+++ tholang/src/app/random-word.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'random-word',
   template:`
-  	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRmf_SBrFnd_0jK9q2NkxT9X4ZiLh1EEWC46WmlOAdK7HWWw/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+  	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRmf_SBrFnd_0jK9q2NkxT9X4ZiLh1EEWC46WmlOAdK7HWWw/viewform?embedded=true" style="position:fixed;top:51px;width:100%;height:90vh" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   `
 })
 
